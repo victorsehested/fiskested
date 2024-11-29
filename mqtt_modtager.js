c24b067b6c3628fcb73d8140b6bfc9c265e5ce15
victor3d <59659352+victorsehested@users.noreply.github.com>
2024-11-29 08:14:33 +0100

    sad

--- mqtt_modtager.js
+++ mqtt_modtager.js
@@ -2,11 +2,9 @@ function modtager(topic, modtagetBesked) {
     let modtagerBuffer = JSON.parse(modtagetBesked);
 	
     let afsenderen = modtagerBuffer.from;
-	if (afsenderen != afsenderID)
-	{
-	    let vol = modtagerBuffer.val;
-	    // do something with the received value
-	}
+	let ReceivedVol = modtagerBuffer.val;
+	// do something with the received value
 	
+	vol = ReceivedVol;
    
 }
